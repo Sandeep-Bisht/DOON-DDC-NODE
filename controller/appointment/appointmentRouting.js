c609db91f4cc03c582cefc26b9ac70cfa3be44fd
Sandeep Bisht <>
2023-06-02 13:42:37 +0530

    handle api of specific period duration

--- controller/appointment/appointmentRouting.js
+++ controller/appointment/appointmentRouting.js
@@ -5,6 +5,9 @@ const router = require("express").Router();
 router.post("/add_appointment", appointmentController.createAppointment);
 router.get('/get_appointment_list',appointmentController.getAllAppointment);
 router.get('/get_specific_date_appointment_list/:date',appointmentController.getSpecificDateAppointment);
+router.get('/get_specific_period_date_appointment_list/from/:fromDate/to/:tillDate',appointmentController.getSpecificPeriodDateAppointment);
+
+
 
 
 module.exports = router;
